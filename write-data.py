480fed8c6b758c769d5b98c29a3e5fbcdacfc17e
Kristian Koehntopp <kris-git@koehntopp.de>
2023-02-27 21:57:01 +0100

    also add a test with commit-per-row.

--- write-data.py
+++ write-data.py
@@ -20,7 +20,7 @@ db = MySQLdb.connect(**db_config)
 def take_time(label, t_old):
     t_new = time.time_ns()
     runtime = t_new - t_old
-    strtime = f"| {label} | {runtime} ns | {runtime/1000000} ms |"
+    strtime = f"| {label} | {runtime} ns | {runtime / 1000000} ms |"
 
     return t_new, strtime
 
@@ -37,6 +37,7 @@ def run_command(label, sql):
 def innodb_yolo_mode():
     set_statement = "set global innodb_flush_log_at_trx_commit = 2"
     run_command("set statement", set_statement)
+
 
 def innodb_acid_mode():
     set_statement = "set global innodb_flush_log_at_trx_commit = 1"
@@ -56,8 +57,9 @@ def create_table():
     )"""
     run_command("create table", sql)
 
+
 def create_data():
-    somestr = "".join(chr(random.randint(0,25) + ord('a')) for x in range(1, 1000))
+    somestr = "".join(chr(random.randint(0, 25) + ord('a')) for x in range(1, 1000))
     insert_statement = "insert into data (x, y, z, data) values (%(x)s, %(y)s, %(z)s, %(data)s)"
 
     innodb_yolo_mode()
@@ -65,26 +67,42 @@ def create_data():
     c = db.cursor()
     d = []
 
-    for i in range(0,64):
-        for j in range(0,64):
-            for k in range(0,64):
-                d.append({'x': i, 'y': j, 'z':k, 'data': somestr})
+    for i in range(0, 64):
+        for j in range(0, 64):
+            for k in range(0, 64):
+                d.append({'x': i, 'y': j, 'z': k, 'data': somestr})
 
     c.executemany(insert_statement, d)
     db.commit()
 
+
 def create_data_slowly():
-    somestr = "".join(chr(random.randint(0,25) + ord('a')) for x in range(1, 1000))
+    somestr = "".join(chr(random.randint(0, 25) + ord('a')) for x in range(1, 1000))
     insert_statement = "insert into data (x, y, z, data) values (%(x)s, %(y)s, %(z)s, %(data)s)"
 
     innodb_yolo_mode()
     c = db.cursor()
     d = []
 
-    for i in range(0,64):
-        for j in range(0,64):
-            for k in range(0,64):
-                c.execute(insert_statement,{'x': i, 'y': j, 'z':k, 'data': somestr})
+    for i in range(0, 64):
+        for j in range(0, 64):
+            for k in range(0, 64):
+                c.execute(insert_statement, {'x': i, 'y': j, 'z': k, 'data': somestr})
+                db.commit()
+
+
+def create_data_very_slowly():
+    somestr = "".join(chr(random.randint(0, 25) + ord('a')) for x in range(1, 1000))
+    insert_statement = "insert into data (x, y, z, data) values (%(x)s, %(y)s, %(z)s, %(data)s)"
+
+    innodb_acid_mode()
+    c = db.cursor()
+    d = []
+
+    for i in range(0, 64):
+        for j in range(0, 64):
+            for k in range(0, 64):
+                c.execute(insert_statement, {'x': i, 'y': j, 'z': k, 'data': somestr})
                 db.commit()
 
 
@@ -106,4 +124,12 @@ print(result)
 
 create_data_slowly()
 t, result = take_time("Write data slowly", t)
+print(result)
+
+create_table()
+t, result = take_time("Recreate table again", t)
+print(result)
+
+create_data_very_slowly()
+t, result = take_time("Write data very slowly", t)
 print(result)
